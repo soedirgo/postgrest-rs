696a9319183f08be5dc9d6f1065881029dad2042
Bobbie Soedirgo <bobbie@soedirgo.dev>
2020-06-08 19:51:03 +0800

    feat: sth
    BREAKING CHANGE: test

--- examples/minimum.rs
+++ examples/minimum.rs
@@ -4,6 +4,6 @@ use postgrest::Postgrest;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let client = Postgrest::new("http://localhost:3000");
     let resp = client.from("todos").select("*").execute().await?;
-    println!("{}", resp.text().await?);
+    println!(" {}", resp.text().await?);
     Ok(())
 }
